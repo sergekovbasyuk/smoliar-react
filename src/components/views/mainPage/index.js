7fa20cd93d8ab60f072923dfbd4ae726b5163920
Serge Kovbasiuk <kolbasovs@gmail.com>
2016-10-19 20:19:55 +0300

    footer on main

--- src/components/views/mainPage/index.js
+++ src/components/views/mainPage/index.js
@@ -103,7 +103,25 @@ class mainPage extends React.Component {
           </div>
         </section>
         <section className="section">
-          {/* <Footer /> */}
+          <div className={s.footerContainer}>
+            <div className={s.contentContacts}>
+              <h1 className={s.footerTitle}>Get in touch</h1>
+              <div className={s.row}>
+                <div className={s.email}>
+                  <p>If you are interested in contacting me, please drop me a line.</p>
+                  <a href="mailto:aasmoliar@gmail.com" className={s.link}>leave me a message</a>
+                </div>
+                <div className={s.schedule}>
+                  <p>I’m busy as usual, so please check my schedule.</p>
+                  <Link to="/contact" className={s.link}>my schedule</Link>
+                </div>
+              </div>
+            </div>
+            <footer className={s.footer}>
+              <a href="/aleksei_smoliar_cv.pdf">Download my CV</a>
+              <a href="http://shortcute.xyz/" target="_blank" rel="noopener noreferrer">created at /shortcute</a>
+            </footer>
+          </div>
         </section>
       </div>
     );
